caffef6065fd009e1349c2c7ed13de46de9ec280
adrianhideki <adrian.hideki.br@gmail.com>
2017-06-21 20:40:06 -0300

    adicionando o frontend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,7 +6,8 @@ const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
-server.use(allowCors)
+server.use
+(allowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is runing on port ${port}.`)
